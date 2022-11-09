248ff3af1cf7fb3d670dbb24301cebdb33c656f5
myungum <gmyungum@gmail.com>
2022-11-10 00:28:32 +0900

    결과 출력 포맷 수정

--- main.py
+++ main.py
@@ -6,16 +6,34 @@ import time
 from tqdm import tqdm
 
 
+class ItemStatistics:
+    def __init__(self) -> None:
+        self.win_cnt = 0
+        self.lose_cnt = 0
+
+    def game_end(self, win):
+        if win:
+            self.win_cnt += 1
+        else:
+            self.lose_cnt += 1
+
+    def __repr__(self) -> str:
+        winning_rate = 100.0 * self.win_cnt / (self.win_cnt + self.lose_cnt)
+        return '{}% ({}승 {}패)'.format(winning_rate, self.win_cnt, self.lose_cnt)
+
+
 with open('settings.json', 'r') as file:
     info = dict(json.load(file))
     client = MongoClient(host=info['db_host'], port=info['db_port'])
     db = client[info['db_name']]
 
 MATCH_SAVED = True
-MATCH_COUNT = 1000
+MATCH_COUNT = 300
 COUNT_PER_QUERY = 100
 DELAY = 1.5  # client can send 50 requests per minute
 ITEM_ID = 1083  # cull(수확의 낫)
+KEY_PURCHASED = '수확의 낫을 산 경우'
+KEY_NOT_PURCHASED = '수확의 낫을 사지 않은 경우'
 
 # 1. get match
 if not MATCH_SAVED:
@@ -45,10 +63,10 @@ for doc in tqdm(list(db['match'].find({'data': None}))):
     print(doc['_id'])
 
 # 3. analyze timeline
-match_list = list(db['match'].find({}))
+match_list = list(db['match'].find({}).sort('matchId', -1))[:MATCH_COUNT]
 result = dict()
-result[True] = [0, 0]
-result[False] = [0, 0]
+result[KEY_PURCHASED] = ItemStatistics()
+result[KEY_NOT_PURCHASED] = ItemStatistics()
 for doc in tqdm(match_list):
     data = doc['data']
 
@@ -62,20 +80,16 @@ for doc in tqdm(match_list):
         raise Exception('participantId not found')
 
     # 3-2. trace item
-    itemFound = False
-
+    key = KEY_NOT_PURCHASED
     winningTeam = -1
     for frame in data['info']['frames']:
         for event in frame['events']:
             if event['type'] == 'ITEM_PURCHASED':
                 if event['itemId'] == ITEM_ID and event['participantId'] == pId:
-                    itemFound = True
+                    key = KEY_PURCHASED
             elif event['type'] == 'GAME_END':
                 winningTeam = event['winningTeam']
     if winningTeam == 100 or winningTeam == 200:
         win = (winningTeam == 100) ^ (pId > 5)
-        if win:
-            result[itemFound][0] += 1
-        else:
-            result[itemFound][1] += 1
+        result[key].game_end(win)
 print(result)
